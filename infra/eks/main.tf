138789312d25ce1eea336fa75f56e761e0fdf207
ooii8929 <ooii8929@gmail.com>
2024-02-11 18:34:14 +0700

    feat: ci/cd

--- infra/eks/main.tf
+++ infra/eks/main.tf
@@ -4,8 +4,13 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    tls = {
+source = "hashicorp/tls"
+version = "3.4.0"
+}
   }
 }
+
 
 # Configure the AWS Provider
 provider "aws" {
